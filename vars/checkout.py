49bf4adf26dc8adc878fb17a241270a36103310f
Prasad Patil <68043685+Prasadrpatil@users.noreply.github.com>
2023-02-17 18:36:03 +0530

    Update checkout.py

--- vars/checkout.py
+++ vars/checkout.py
@@ -1,21 +1,6 @@
-import os
-from git import Repo
+import git
 
-def git_checkout(url, branch, path):
-    """Checkout a Git repository at the specified branch and path."""
-    # Construct the full URL of the Git repository.
-    full_url = f"{url}#{branch}"
+repo_url = 'https://github.com/user/repo.git'
+repo_path = '/path/to/clone/repo'
 
-    # Ensure the target directory exists.
-    os.makedirs(path, exist_ok=True)
-
-    # Clone or fetch the Git repository.
-    if os.path.exists(os.path.join(path, '.git')):
-        repo = Repo(path)
-        repo.remote().fetch()
-    else:
-        Repo.clone_from(full_url, path, branch=branch)
-
-    # Checkout the desired branch.
-    repo = Repo(path)
-    repo.git.checkout(branch)
+repo = git.Repo.clone_from(repo_url, repo_path)
