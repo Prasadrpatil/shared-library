41d6d7b41ead2e8fba37a160b75c3a9532b6e5d9
Prasad Patil <68043685+Prasadrpatil@users.noreply.github.com>
2023-02-20 11:57:23 +0530

    Update checkout.py

--- vars/checkout.py
+++ vars/checkout.py
@@ -1,13 +1,6 @@
-import os
 import git
 
-directory = '/repo'
-
-if not os.path.exists(directory):
-    os.makedirs(directory)
-else:
-    print(f"The directory '{directory}' already exists.")
-
 repo_url = 'https://github.com/Prasadrpatil/java-calculator.git'
+directory = '/repo'
 
 repo = git.Repo.clone_from(repo_url, directory)
