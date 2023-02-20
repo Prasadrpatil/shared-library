7a89e3bcc9c9739831517fe8a8897da32a2b6ae1
Prasad Patil <68043685+Prasadrpatil@users.noreply.github.com>
2023-02-20 12:15:15 +0530

    Update checkout.py

--- vars/checkout.py
+++ vars/checkout.py
@@ -1,8 +1,9 @@
 import git
 import argparse
 
-parser.add_argument('repository_url', type=str, help='The URL of the Git repository to clone')
-parser.add_argument('target_directory', type=str, help='The target directory for the cloned repository')
+parser = argparse.ArgumentParser(description='Clone a Git repository')
+parser.add_argument('repository_url', type=str)
+parser.add_argument('target_directory', type=str)
 args = parser.parse_args()
 
 # repo_url = 'https://github.com/Prasadrpatil/java-calculator.git'
