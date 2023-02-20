ad98ffc92b14b6c3469cbdb349591e34465d6fee
Prasad Patil <68043685+Prasadrpatil@users.noreply.github.com>
2023-02-20 11:48:55 +0530

    Update checkout.py

--- vars/checkout.py
+++ vars/checkout.py
@@ -4,4 +4,4 @@ import git
 repo_url = 'https://github.com/Prasadrpatil/java-calculator.git'
 repo_path = '/path'
 
-repo = git.Repo.clone_from(repo_url, repo_path)
+repo = git.Repo.clone_from(repo_url)
