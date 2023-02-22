a45693187a8b4793f317afc04424ca6f6bc06edd
Prasad Patil <68043685+Prasadrpatil@users.noreply.github.com>
2023-02-22 15:34:04 +0530

    Create sonarqubeAnalysis.groovy

---
+++ vars/sonarqubeAnalysis.groovy
@@ -0,0 +1,13 @@
+def call(Map config = [:]) { 
+      withSonarQubeEnv('sonarqube') {
+					sh 'mvn sonar:sonar \
+              -Dsonar.projectKey=config.project \
+              -Dsonar.host.url=config.url \
+              -Dsonar.login=config.token'
+			}   
+}
+
+
+
+
+
