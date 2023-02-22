1d185d19103c40ddb912ea09707d414de0ec12c8
Prasad Patil <68043685+Prasadrpatil@users.noreply.github.com>
2023-02-22 13:02:07 +0530

    Update secretDetect.groovy

--- vars/secretDetect.groovy
+++ vars/secretDetect.groovy
@@ -1,4 +1,6 @@
 def call(Map config = [:]) {
          
-     sh 'trufflehog --no-update filesystem --directory=/var/lib/jenkins/workspace/Devsecops-Pipeline/ --debug --json > trufflehog.json'
+         sh 'pwd'
+         sh 'whispers . > whipsers.json'
+//      sh 'trufflehog --no-update filesystem --directory=/var/lib/jenkins/workspace/Devsecops-Pipeline/ --debug --json > trufflehog.json'
 }
