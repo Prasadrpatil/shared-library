9de968c99ed5f2249923ea40850b734053246ae5
Prasad Patil <68043685+Prasadrpatil@users.noreply.github.com>
2023-02-22 10:04:30 +0530

    Update staticCodeAnalysis.groovy

--- vars/staticCodeAnalysis.groovy
+++ vars/staticCodeAnalysis.groovy
@@ -1,4 +1,10 @@
 def call(Map config = [:]) {     
-     sh 'snyk auth b369ec32-7576-4822-95ca-e59b0cb76dc4'    
-     sh 'snyk test /var/lib/jenkins/workspace/Devsecops-Pipeline/ --json > snyk.json'
+     snykSecurity(
+            snykInstallation: 'snyk',
+            snykTokenId: 'Snyk-Token',
+            failOnIssues: false,
+            additionalArguments: '--json-file-output=snyk-report.json'
+      )
+//      sh 'snyk auth b369ec32-7576-4822-95ca-e59b0cb76dc4'    
+//      sh 'snyk test /var/lib/jenkins/workspace/Devsecops-Pipeline/ --json > snyk.json'
 }
