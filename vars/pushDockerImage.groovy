1ad769f21ef2c56b0f37a55bfd8d440e9c71b116
Prasad Patil <68043685+Prasadrpatil@users.noreply.github.com>
2023-02-28 10:48:13 +0530

    Update pushDockerImage.groovy

--- vars/pushDockerImage.groovy
+++ vars/pushDockerImage.groovy
@@ -1,6 +1,5 @@
 def call(Map config = [:]) {
        docker.withRegistry( '', registryCredential ) {
-              dockerImage = docker.image(config.imageName)
               dockerImage.push()
 	}
 }
