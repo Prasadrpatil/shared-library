e93e1b3a9d916f259bbf8ff4c6d0e2031cd89172
Prasad Patil <68043685+Prasadrpatil@users.noreply.github.com>
2023-03-03 11:31:29 +0530

    Update checkout.py

--- vars/checkout.py
+++ vars/checkout.py
@@ -1,12 +1,1 @@
-import git
-import argparse
-
-parser = argparse.ArgumentParser(description='Clone a Git repository')
-parser.add_argument('repository_url', type=str, help='The URL of the Git repository to clone')
-parser.add_argument('target_directory', type=str, help='The target directory for the cloned repository')
-args = parser.parse_args()
-
-# repo_url = 'https://github.com/Prasadrpatil/java-calculator.git'
-# directory = '/repo'
-
-repo = git.Repo.clone_from('https://github.com/Prasadrpatil/java-calculator', '/var/lib/jenkins/workspace/Devsecops-Pipeline/repo')
+print("hello from prasad")
