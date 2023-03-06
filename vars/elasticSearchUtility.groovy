cedc26fdf65763f13a5db554e01d98bbc9929019
Prasad Patil <68043685+Prasadrpatil@users.noreply.github.com>
2023-03-06 10:41:30 +0530

    Update elasticSearchUtility.groovy

--- vars/elasticSearchUtility.groovy
+++ vars/elasticSearchUtility.groovy
@@ -3,5 +3,5 @@ pythonContent = libraryResource('dockerImageScan.py')
 
 writeFile(file: 'dockerImageScan.py', text: pythonContent)
 sh('chmod +x dockerImageScan.py')
-sh('/usr/bin/python3 dockerImageScan.py')
+sh('python3 dockerImageScan.py')
 }
