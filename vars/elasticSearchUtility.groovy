e7f6e54a8b6ed7bcf0be9fef5fd7f75254f3d71c
Prasad Patil <68043685+Prasadrpatil@users.noreply.github.com>
2023-03-06 15:22:53 +0530

    Update elasticSearchUtility.groovy

--- vars/elasticSearchUtility.groovy
+++ vars/elasticSearchUtility.groovy
@@ -21,3 +21,11 @@ writeFile(file: 'dockerFileScan.py', text: pythonContent)
 sh('chmod +x dockerFileScan.py')
 sh('python3 dockerFileScan.py')
 }
+
+def k8ManifestScan() {
+pythonContent = libraryResource('k8ManifestScan.py')
+
+writeFile(file: 'k8ManifestScan.py', text: pythonContent)
+sh('chmod +x k8ManifestScan.py')
+sh('python3 k8ManifestScan.py')
+}
