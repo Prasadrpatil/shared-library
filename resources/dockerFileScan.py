0893228f33ed979847fcebf3080ea1c2ae85e441
Prasad Patil <68043685+Prasadrpatil@users.noreply.github.com>
2023-03-06 15:12:32 +0530

    Create dockerFileScan.py

---
+++ resources/dockerFileScan.py
@@ -0,0 +1,52 @@
+import json
+import os
+from elasticsearch import Elasticsearch
+
+with open('dockerfile-scan .json', 'r') as f:
+    # data1 = f.read().strip() 
+    log_data = json.load(f)
+
+
+filtered_log_list = []
+
+# Opening the json file
+# with open('trivy.json') as f:
+#     log_data = json.load(f)
+
+filtered_log_list.append({
+        'ArtifactName': log_data['ArtifactName'],         
+    })
+    
+# filterd data and append on list
+for i in range(0, len(log_data['Results'])):
+    for j in range(0, len(log_data['Results'][i]['Misconfigurations'])):
+        filtered_log_list.append(
+                        {
+                            'Title': log_data['Results'][i]['Misconfigurations'][j]['Title'],
+                            # 'Target' : log_data['Results'][i]['Vulnerabilities'][j]['Target'],
+                            'Description': log_data['Results'][i]['Misconfigurations'][j]['Description'],
+                            'Severity': log_data['Results'][i]['Misconfigurations'][j]['Severity'],
+                            'Resolution' : log_data['Results'][i]['Misconfigurations'][j]['Resolution'],
+                            'Message' : log_data['Results'][i]['Misconfigurations'][j]['Message'],
+                            'Reference' : log_data['Results'][i]['Misconfigurations'][j]['References'],
+                            'Status' : log_data['Results'][i]['Misconfigurations'][j]['Status']
+                        }
+                        ) 
+print(filtered_log_list)
+#duming the list on the json file
+with open('dockerfile-Scanningdump.json','w') as f:
+        # json.dump(filtered_log_list,f)
+        json.dump(filtered_log_list,f)
+#closing the opened file
+f.close()
+
+es = Elasticsearch(['http://3.208.12.113:9200'])
+
+with open('dockerfile-Scanningdump.json', 'r') as f:
+#     # data1 = f.read().strip() 
+     data = json.load(f)
+
+for doc in data:
+    res = es.index(index="docker-scanning", body=doc)
+    
+print("DATA IS PUSHED TO ELASTICSEARCH !!!")
