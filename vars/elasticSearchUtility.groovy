31004d4a3e2409bda2540cfbb8923a41267b6cb1
Prasad Patil <68043685+Prasadrpatil@users.noreply.github.com>
2023-03-06 11:38:56 +0530

    Update elasticSearchUtility.groovy

--- vars/elasticSearchUtility.groovy
+++ vars/elasticSearchUtility.groovy
@@ -6,3 +6,12 @@ writeFile(file: 'dockerImageScan.py', text: pythonContent)
 sh('chmod +x dockerImageScan.py')
 sh('python3 dockerImageScan.py')
 }
+
+def sbomScan() {
+// sh 'pip3 install urllib3'
+pythonContent = libraryResource('sbomScanning.py')
+
+writeFile(file: 'sbomScanning.py', text: pythonContent)
+sh('chmod +x sbomScanning.py')
+sh('python3 sbomScanning.py')
+}
