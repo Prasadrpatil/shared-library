71e94907a78b3a905e9c1689bbb264deb457ccfa
Prasad Patil <68043685+Prasadrpatil@users.noreply.github.com>
2023-03-10 09:13:49 +0530

    Update sonarqubeAnalysis.groovy

--- vars/sonarqubeAnalysis.groovy
+++ vars/sonarqubeAnalysis.groovy
@@ -2,7 +2,7 @@ def call(Map config = [:]) {
       withSonarQubeEnv('sonarqube') {
            sh 'mvn sonar:sonar \
               -Dsonar.projectKey=java-proj \
-              -Dsonar.host.url=http://34.228.16.90:9000 \
+              -Dsonar.host.url=http://34.207.149.238:9000 \
               -Dsonar.login=sqp_e025a02e02bffe81d48161d1accf1219ff79f659'
       }
 }
